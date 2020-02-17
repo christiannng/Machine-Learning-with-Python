3123ac07f4cd7d174b7c617203f4ee33d640e851
Christian Nguyen <christiannguyen110591@gmail.com>
2020-02-16 21:17:14 -0500

    [Christian] Multiple linear regression and the process of backward elimination

---
+++ Multiple Linear Regression/multiple_linear_regression_backward_elimination.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 16 20:30:01 2020
+
+@author: Christian
+"""
+
+#Multiple Linear Regression with backward elimination
+#What if some of the column is significant to the model
+##While some of them are not as significant as? Backward elimination
+
+#Importing the libraries
+import numpy as np #Mathematical library
+import matplotlib.pyplot as plt #To plot nice chart
+import pandas as pd #To import and manage dataset
+
+#Import the dataset
+dataset = pd.read_csv('50_Startups.csv')
+#Matrix of independent variables: X = independent vars; y = dependent vars
+X = dataset.iloc[:, :-1].values #Getting data from 3 columns in dataset
+y = dataset.iloc[:, 4].values #Getting data from the 4th column (index start at 0)
+
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+##Encoding independent variables
+labelencoder_X = LabelEncoder()
+#However, this could cause the problem where our algo would think that 
+##French has higher value than Germany, etc.
+##To solve this, we would have 3 columns represent 0 and 1
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3]) 
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
+#Avoiding the dummy variable trap
+#Usually the library do this for uus
+X = X[:, 1:]
+
+
+#Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+#We split test set and train set 80-20
+X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                    y, 
+                                                    test_size = 0.2,
+                                                    random_state = 0)
+
+#Fitting multiple linear regression to the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+#Building the optimal model using backward elimination
+##Step 1 of backward elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+##Step 2 of backward elimination
+X_optimal = X[:, [0,1,2,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_optimal).fit()
+##Step 3, 4, 5 of backward elimination
+##TO check the P-value, the lower the more significant
+regressor_OLS.summary()
+
+X_optimal = X[:, [0,1,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_optimal).fit()
+##Step 3, 4, 5 of backward elimination
+##TO check the P-value, the lower the more significant
+regressor_OLS.summary()
+
+X_optimal = X[:, [0,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_optimal).fit()
+##Step 3, 4, 5 of backward elimination
+##TO check the P-value, the lower the more significant
+regressor_OLS.summary()
+
+X_optimal = X[:, [0,3,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_optimal).fit()
+##Step 3, 4, 5 of backward elimination
+##TO check the P-value, the lower the more significant
+regressor_OLS.summary()
+
+X_optimal = X[:, [0,3]]
+regressor_OLS = sm.OLS(endog = y, exog = X_optimal).fit()
+##Step 3, 4, 5 of backward elimination
+##TO check the P-value, the lower the more significant
+regressor_OLS.summary()
+
+
+
+
+
+
+
+
+
